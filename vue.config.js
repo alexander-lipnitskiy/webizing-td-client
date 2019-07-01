0989761fda74eb432530c0186783ef856a732d7e
Lipnitskiy Alexander <lipnitskiy.alexander@gmail.com>
2019-07-01 14:04:21 +0900

    add docker file for production

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/Users/alexander/Documents/GitHub/webizing-td-client/dist/' : '/'
-};
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production' ? '/Users/alexander/Documents/GitHub/webizing-td-client/dist/' : '/'
+// };
