8be64eff0195ba9c29aec2fd77e0477bf9395448
Lipnitskiy Alexander <lipnitskiy.alexander@gmail.com>
2019-10-29 17:45:03 +0900

    add base /td path

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === 'production' ? '/Users/alexander/Documents/GitHub/webizing-td-client/dist/' : '/'
-// };
+module.exports = {
+    baseUrl: '/td',
+    publicPath: '/td'
+};
