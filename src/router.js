f4bda273c82fbda772aea7b2c9d920708727bfb8
Lipnitskiy Alexander <lipnitskiy.alexander@gmail.com>
2019-10-29 21:25:23 +0900

    change bugs with root url for production

--- src/router.js
+++ src/router.js
@@ -11,7 +11,8 @@ Vue.use(Router);
 
 export default new Router({
   // mode: 'history',
-  base: process.env.BASE_URL,
+  base: '/td',
+  mode: 'history',
   routes: [
     {
       path: '/',
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-module.exports = {
-    baseUrl: '/td',
-    publicPath: '/td'
-};
+// module.exports = {
+//     publicPath: process.env.NODE_ENV === 'production'
+//         ? '/td/'
+//         : '/'
+// };
